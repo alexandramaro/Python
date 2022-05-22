88a70ed026695ec858bb62779433d4fe9b35f56b
Alexandra Amaro <105989301+AlexandrAmaro@users.noreply.github.com>
2022-05-22 20:31:00 +0100

    update exercicios

---
+++ MODULOS 1, 2 e 3/Exercicios/ex088_PalpitesTotoloto.py
@@ -0,0 +1,32 @@
+""" Faça um programa que ajude um jogador do Totoloto (Mega Sena), a criar palpites.
+O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60
+para cada jogo, registando tudo em uma lista composta."""
+
+from random import randint
+from time import sleep
+lista = list()
+jogos = list()
+print('-' * 30)
+print('       JOGA NO TOTOLOTO')
+print('-' * 30)
+quant = int(input('Quantos jogos quer sortear? '))
+tot = 1
+while tot <= quant:
+    count = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            count += 1
+        if count >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+print('-=' * 3, f'SORTEANDO {quant} JOGOS', '-=' * 3)
+# aparece lista de jogos ordenados verticalmente
+for indice, lista in enumerate(jogos):
+    print(f'Jogo {indice + 1}: {lista}')
+    sleep(1)
+print('-=' * 5, '< BOA SORTE! >', '-=' * 5)
