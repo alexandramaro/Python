df76f262808a070d43cfcf5baf2e2e216be026d9
Alexandra Amaro <105989301+AlexandrAmaro@users.noreply.github.com>
2022-05-24 19:27:35 +0100

    ex090

--- MODULOS 1, 2 e 3/Exercicios/ex090_DicionarioNomeMedia.py
+++ MODULOS 1, 2 e 3/Exercicios/ex090_DicionarioNomeMedia.py
@@ -1,12 +1,16 @@
 """ ex090 Faça um programa que leia nome e média de um aluno, guardando também
-a situação num dicionário. No final, mostre o conteúdo da estrutura na tela."""
+a situação (aprovado, reprovado, recuperação) num dicionário.
+No final, mostre o conteúdo da estrutura na tela."""
 
 aluno = dict()
-alunos = list()
-for count in range(0, 3):
-    aluno['nome'] = str(input('Nome do aluno: '))
-    aluno['média'] = float(input('Média do aluno: '))
-    alunos.append(aluno.copy())
-for a in aluno:
-    for k, v in alunos:
-        print(f'O aluno {k} teve média de {v}]}')
+aluno['nome'] = str(input('Nome do aluno: '))
+aluno['média'] = float(input(f'Média do {aluno["nome"]}: '))
+if aluno['média'] >= 10:
+    aluno['situação'] = 'Aprovado'
+elif 9.5 <= aluno['média'] < 10:
+    aluno['situação'] = 'Recuperação'
+else:
+    aluno['situação'] = 'Reprovado'
+print('-=' * 30)
+for k, v in aluno.items():
+    print(f'    - {k} é igual a \033[34m{v}\033[m')
