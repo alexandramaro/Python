c262ab44883ced603bd8f39a671fdc368d94b258
Alexandra Amaro <105989301+AlexandrAmaro@users.noreply.github.com>
2022-05-24 23:18:12 +0100

    Create ex093_RegistarJogadorFutebol.py

---
+++ MODULOS 1, 2 e 3/Exercicios/ex093_RegistarJogadorFutebol.py
@@ -0,0 +1,5 @@
+""" ex093 Crie um programa que gerencie o aproveitamento de um jogador de futebol.
+O programa vai ler o nome do jogador e quantas partidas ele jogou.
+Depois vai ler a quantidade de golos feitos em cada partida.
+No final, tudo isso será guardado num dicionário, incluindo o total de golos feitos
+durante o campeonato."""
