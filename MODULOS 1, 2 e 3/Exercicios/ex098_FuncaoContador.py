65f8d676b1a6cb9d0c8865571d56b550b7a2341d
Alexandra Amaro <105989301+AlexandrAmaro@users.noreply.github.com>
2022-05-29 18:16:55 +0100

    workspace

---
+++ MODULOS 1, 2 e 3/Exercicios/ex098_FuncaoContador.py
@@ -0,0 +1,9 @@
+""" ex098 Faça um programa que tenha uma função chamada contador(), que receba
+três parâmetros: início, fim e passo e realize a contagem.
+O programa tem que realizar três contagens através da função criada:
+a) De 1 até 10, de 1 em 1.
+b) De 10 até 0, de 2 em 2.
+c) Uma contagem personalizada."""
+
+
+def contador():
