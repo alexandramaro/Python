37430290abae3c9e5da83fe707b859c62dfc4649
Alexandra Amaro <105989301+AlexandrAmaro@users.noreply.github.com>
2022-06-05 19:37:36 +0100

    update 101

--- MODULOS 1, 2 e 3/Exercicios/ex101_FuncoesParaVotacoes.py
+++ MODULOS 1, 2 e 3/Exercicios/ex101_FuncoesParaVotacoes.py
@@ -12,7 +12,7 @@ def voto(ano):
         return f'Com {idade} anos: VOTO OPCIONAL!'
     else:
         return f'Com {idade} anos: VOTO OBRIGATÓRIO!'
-    
+
 
 # Programa Principal
 nasc = int(input('Em que ano você nasceu?'))
