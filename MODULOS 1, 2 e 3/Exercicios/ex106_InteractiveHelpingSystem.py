50382390690b01eaa901d06d385b81ee3e8e35e1
Alexandra Amaro <105989301+AlexandrAmaro@users.noreply.github.com>
2022-06-05 20:46:39 +0100

    Create ex106_InteractiveHelpingSystem.py

---
+++ MODULOS 1, 2 e 3/Exercicios/ex106_InteractiveHelpingSystem.py
@@ -0,0 +1,4 @@
+""" Ex106: Faça um mini-sistema que utilize o Interactive Help do Python. O utilizador vai
+digitar o comando e o manual vai aparecer. Quando o utilizador escrever a palavra 'FIM',:keywordprograma encerra.
+OBS: usar cores."""
+
