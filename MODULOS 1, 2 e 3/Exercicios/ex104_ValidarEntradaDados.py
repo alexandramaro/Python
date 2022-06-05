e258f3b1fadd308721e5bf854af4217010400c88
Alexandra Amaro <105989301+AlexandrAmaro@users.noreply.github.com>
2022-06-05 20:07:00 +0100

    correção ex103

---
+++ MODULOS 1, 2 e 3/Exercicios/ex104_ValidarEntradaDados.py
@@ -0,0 +1,22 @@
+""" Ex104: Crie um programa que tenha uma função leiaInt(), que vai funcionar de forma
+semelhante à função input do Python, porém fazendo a validação para aceitar apenas
+um valor numérico.
+Ex: n = leiaInt('Digite um nº: ')"""
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[031mERRO! Escreva um número inteiro válido.\033[m')
+        if ok:
+            break
+    return valor
+
+
+# Programa Principal
+n = leiaInt('Escreva um número:')       #  n = int(input('Escreva um número:'))
+print(f'Você acabou de escrever o número {n}.')
