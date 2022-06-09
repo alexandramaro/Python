c1a9569d5eefc440064e6f3cd12b256ef1d73483
Alexandra Amaro <105989301+AlexandrAmaro@users.noreply.github.com>
2022-06-09 20:56:12 +0100

    update

--- MODULOS 1, 2 e 3/Exercicios/ex110_ReduzindoAindaMaiso109/moeda.py
+++ MODULOS 1, 2 e 3/Exercicios/ex110_ReduzindoAindaMaiso109/moeda.py
@@ -7,7 +7,7 @@ def aumentar(preco=0, taxa=0, formato=False):
     """
     → Calcula o aumento de um determinado preço, retornando o resultado com ou sem formatação.
     :param preco: € o preço que se quer reajustar
-    :param taxa: a percentagem do aumento (10% e 13%)
+    :param taxa: a percentagem do aumento
     :param formato: Saída formatada (,00€) ou não?
     :return: o valor reajustado, com ou sem formato.
     """
@@ -32,3 +32,15 @@ def metade(preco=0, formato=False):
 
 def moeda(preco=0, moeda='€'):
     return f'{preco:>.2f}{moeda}'.replace('.', ',')
+
+
+def resumo(preco=0, taxaa=10, taxar=5):
+    print('-' * 30)
+    print('RESUMO DO VALOR'.center(30))
+    print('-' * 30)
+    print(f'Preço analisado: \t{moeda(preco)}')
+    print(f'Dobro do preço: \t{dobro(preco, True)}')
+    print(f'Metade do preço: \t{metade(preco, True)}')
+    print(f'{taxaa}% de aumento: \t{aumentar(preco, taxaa, True)}')
+    print(f'{taxar}% de redução: \t{diminuir(preco, taxaa, True)}')
+    print('-' * 30)
--- MODULOS 1, 2 e 3/Exercicios/ex110_ReduzindoAindaMaiso109/teste.py
+++ MODULOS 1, 2 e 3/Exercicios/ex110_ReduzindoAindaMaiso109/teste.py
@@ -1,7 +1,4 @@
-from ex109_FormatandoMoedasMelhorado import moeda
+from ex110_ReduzindoAindaMaiso109 import moeda
 
 p = float(input('Digite o preço: €'))
-print(f'A metade de {moeda.moeda(p)} é {moeda.metade(p, True)}')    # → True da formatação da moeda
-print(f'O dobro de {moeda.moeda(p)} é {moeda.dobro(p, True)}')
-print(f'Aumentando 10%, temos {moeda.aumentar(p, 10, True)}')
-print(f'Reduzindo 13%, temos {moeda.diminuir(p, 13, True)}')
+moeda.resumo(p, 20, 12)
