01ee4f440507c01b891cfc432d502eac83481f8d
Alexandra Amaro <105989301+AlexandrAmaro@users.noreply.github.com>
2022-06-09 20:31:28 +0100

    ex109

---
+++ MODULOS 1, 2 e 3/Exercicios/ex109_FormatandoMoedasMelhorado/moeda.py
@@ -0,0 +1,34 @@
+""" ex109: Modifique as funções que foram criadas no desafio 107 para que elas aceitem
+um parâmetro a mais, informando se o valor retornado por elas, vai ou não ser formatado
+pela função moeda(), desenvolvida no desafio 108."""
+
+
+def aumentar(preco=0, taxa=0, formato=False):
+    """
+
+    :param preco: €
+    :param taxa: 10% e 13%
+    :param formato: ,00€
+    :return: res if
+    """
+    res = preco + (preco * taxa / 100)
+    return res if formato is False else moeda(res)
+
+
+def diminuir(preco=0, taxa=0, formato=False):
+    res = preco - (preco * taxa / 100)
+    return res if not formato else moeda(res)
+
+
+def dobro(preco=0, formato=False):
+    res = preco * 2
+    return res if not formato else moeda(res)
+
+
+def metade(preco=0, formato=False):
+    res = preco / 2
+    return res if not formato else moeda(res)
+
+
+def moeda(preco=0, moeda='€'):
+    return f'{preco:>.2f}{moeda}'.replace('.', ',')
